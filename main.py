2719c021e4309b21fd9fc1a0cb2d2fe92362945b
de3panshu <sahu.deeepanshu.united@gmail.com>
2025-03-21 15:38:55 +0530

    add spanish greet

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import os
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def greetDefault():
     return {"message": "Hello, welcome to the greeting API!"}
 
 @app.get("/japan")
-def read_root():
+def greetJapan():
     return {"message": "Konnichiwa"}
 
+@app.get("/spanish")
+def greetSpanish():
+    return {"message": "Hola"}
+
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT env var is not set
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
